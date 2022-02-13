3fd2b262eb926620c805efce6d75d860e970c3b1
aminnairi <18418459+aminnairi@users.noreply.github.com>
2022-02-13 20:53:29 +0100

    removed build options

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,6 @@ import {resolve} from "path";
 
 export default {
   root: resolve("example", "router"),
-  build: {
-    outDir: resolve("build"),
-    emptyOutDir: true
-  },
   server: {
     port: 8000,
     host: "0.0.0.0"
