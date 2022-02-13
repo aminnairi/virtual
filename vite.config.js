40957c676bc22011b8f5ca920fa471ea687bf635
aminnairi <18418459+aminnairi@users.noreply.github.com>
2022-02-13 20:49:47 +0100

    build options

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import {resolve} from "path";
 
 export default {
   root: resolve("example", "router"),
+  build: {
+    outDir: resolve("build"),
+    emptyOutDir: true
+  },
   server: {
     port: 8000,
     host: "0.0.0.0"
